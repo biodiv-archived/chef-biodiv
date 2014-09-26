bb702935ccbd19a581b93458676c0ca11ebe5142
sandeeptandekar <sandeept@strandls.com>
2014-09-26 17:45:07 +0530

    cleanup data dir of geoserver so that it initialize with observation_locations layer

--- chef-repo/cookbooks/geoserver/recipes/database.rb
+++ chef-repo/cookbooks/geoserver/recipes/database.rb
@@ -11,6 +11,7 @@ postgresql_connection_info = {:host => "localhost",
 postgresql_database node.geoserver.database do
   connection postgresql_connection_info
   action :create
+  notifies :query, "postgresql_database[create postgis extension]"
 end
 
 
@@ -20,6 +21,15 @@ postgresql_database_user node['geoserver']['database-user'] do
   password node['geoserver']['database-password']
   action :create
 end
+
+# setup postgis extension
+postgresql_database 'create postgis extension' do
+  connection      postgresql_connection_info
+  database_name node.geoserver.database
+  sql "CREATE EXTENSION if not exists postgis; CREATE EXTENSION if not exists postgis_topology; CREATE EXTENSION if not exists dblink;create or replace view observation_locations as SELECT obs.id, obs.source, obs.species_name, obs.topology FROM dblink('dbname=biodiv'::text, 'select id, source, species_name, topology from observation_locations'::text) obs(id bigint, source text, species_name character varying(255), topology geometry);"
+  action :nothing
+end
+
 
 # grant all privileges on all tables in foo db
 postgresql_database_user node['geoserver']['database-user'] do
